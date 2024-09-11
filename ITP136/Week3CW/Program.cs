c3d2a05c27486cdf5db7c597bf96fac7c732e0d0
Timothy Quinn <chopperkid8@hotmail.com>
2024-09-11 18:53:48 -0400

    Testing 1

---
+++ ITP136/Week3CW/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Week3CW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+
+            string inputString;
+
+            Console.WriteLine("Enter your name: ");
+            inputString = Console.ReadLine();
+
+            Console.WriteLine("Enter your first number: ");
+            inputString = Console.ReadLine();
+            a = Convert.ToDouble(inputString);
+
+            Console.WriteLine("Enter your second number: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+
+            double total = a + b;
+            Console.WriteLine(total);
+
+        }
+    }
+}
