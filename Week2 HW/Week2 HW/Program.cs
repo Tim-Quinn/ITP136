9bd4b825e2820d8beb654a005eb2b10b3b0b378f
Timothy Quinn <chopperkid8@hotmail.com>
2024-09-14 12:43:16 -0400

    Week 2 HW

---
+++ Week2 HW/Week2 HW/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Week2_HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string patientNname;
+            Console.WriteLine("Who is being seen today?: ");
+            patientNname = Console.ReadLine();
+
+            Console.WriteLine("Welcome: " + patientNname);
+        }
+    }
+}
